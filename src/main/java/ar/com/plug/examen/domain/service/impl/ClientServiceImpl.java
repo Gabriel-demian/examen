d5e4b22656e7811df2ce68d46e59e014c3ba714b
Gabriel <gabriel.leoni.um@gmail.com>
2021-01-05 22:59:13 -0300

    Update ClientServiceImpl.java

--- src/main/java/ar/com/plug/examen/domain/service/impl/ClientServiceImpl.java
+++ src/main/java/ar/com/plug/examen/domain/service/impl/ClientServiceImpl.java
@@ -6,44 +6,61 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import ar.com.plug.examen.app.api.ClientApi;
+import ar.com.plug.examen.domain.mappers.ClientMapper;
+import ar.com.plug.examen.domain.model.Client;
 import ar.com.plug.examen.domain.repositories.ClientRepository;
 import ar.com.plug.examen.domain.service.ClientService;
+import lombok.extern.java.Log;
 
+@Log
 @Service
 public class ClientServiceImpl implements ClientService{
 	
 	@Autowired
 	private ClientRepository clientRepository;
-	
+	@Autowired
+	private ClientMapper clientMapper;
 
+	
 	@Override
 	public ClientApi createClient(ClientApi clientApi) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+
+		Client client = clientRepository.save(clientMapper.fillEntity(new Client(), clientApi));
+		
+		log.severe("The client " + client.getId() +" was succesfully created.");
+		
+		return clientMapper.getDto(client);
+	}  
 
 	@Override
 	public ClientApi getClient(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Client client = clientRepository.findById(id).orElse(null);
+		
+		return clientMapper.getDto(client);
 	}
 
 	@Override
 	public List<ClientApi> listAllClients() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		List<Client> client = clientRepository.findAll();
+		
+		return clientMapper.getDto(client);
 	}
 
 	@Override
 	public void removeClient(Long id) {
-		// TODO Auto-generated method stub
+		
+		clientRepository.deleteById(id);
 		
 	}
 
 	@Override
 	public ClientApi updateClient(Long id, ClientApi clientApi) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Client client = clientRepository.save(clientMapper.fillEntity(new Client(), clientApi));
+		
+		return clientMapper.getDto(client);
 	}
 
 }
